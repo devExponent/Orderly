1dfb60a323bd1b240b424ef69390dcdf0bcb0476
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2026-01-10 23:36:47 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <p>Hello</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/store/Foodstore.tsx
@@ -0,0 +1,10 @@
+// import { useContext, useState, useEffect } from "react";
+// export const FoodContext = useContext({});
+// export const FoodstoreProvider = ({children}) => {
+//   const [foodstore, setFoodStore] = useState();
+
+//   const contextValue={
+
+//   }
+//   return <FoodContext>{children}</FoodContext>;
+// };
