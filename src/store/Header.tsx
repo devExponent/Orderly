6efb6d91bd34a30a977118ce060300bb85ee018a
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2026-01-12 16:00:59 +0200

    Logo

--- src/store/Header.tsx
+++ src/store/Header.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import Logo from "/logo.jpg";
 
 const Header = () => {
   return (
     <div className="flex justify-around">
-      <div className="flex items-center"></div>
+      <div className="flex items-center">
+        <img src={Logo} alt="logo" />
+      </div>
       <div>fkmfkmmn</div>
     </div>
   );
